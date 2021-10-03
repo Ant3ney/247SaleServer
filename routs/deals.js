80a60fa09af31225ce3fe51b4d8107cf7f1114fa
Anthony Cavuoti <anthonycavuoti@gmail.com>
2021-10-02 18:26:57 -0700

    Add code to avoid crash when a featured game dosent exzist

--- routs/deals.js
+++ routs/deals.js
@@ -1,25 +1,36 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 let SpecialDeals = require('../models/specialDealIndexes');
 
 router.get('/special', (req, res) => {
-    console.log('In specia; deals');
-    SpecialDeals.find({}, (err, sDeals) => {
-        if(err){
-            let message = 'Something went wrong in special deals rout DB function because ' + err.message;
-            console.error(message);
+   console.log('In special deals');
+   SpecialDeals.find({}, (err, sDeals) => {
+      if (err) {
+         let message =
+            'Something went wrong in special deals rout DB function because ' +
+            err.message;
+         console.error(message);
+         res.status(500).json({
+            message: message,
+            failed: true,
+         });
+      } else {
+         sDeal = sDeals[0];
+         console.log(sDeal);
+         if (!sDeal) {
+            console.log('Failed to create special deal');
             res.status(500).json({
-                message: message
+               message: 'Server Error, Nothing was returned in special deal',
+               failed: true,
             });
-        }
-        else{
-            sDeal = sDeals[0];
-            console.log('Sending deals');
-            res.status(200).json({
-                sDeal
-            });
-        }
-    });
-})
+            return;
+         }
+         console.log('Sending deals');
+         res.status(200).json({
+            sDeal,
+         });
+      }
+   });
+});
 
 module.exports = router;
--- utilitys/cronJobs.js
+++ utilitys/cronJobs.js
@@ -8,133 +8,149 @@ let saleIndexes = [];
 let globalDeals = {};
 
 exports.dayCron = async () => {
-    getAndSetSpecialDealIndexes();
-}
+   getAndSetSpecialDealIndexes();
+};
 
 let getAndSetSpecialDealIndexes = async () => {
-    console.log('running a task every day');
-    console.log('Getting deal length');
-    let length = await getDealAndLength();
-    console.log('length: ' + length);
-    for(let i = 0; i < 4; i++){
-        addNumber(length);
-    }
-    SpecialDeals.remove({}, async err => {
-        if(err){
-            console.error('Something wnet wrong when deleteing SpecialDealsIndexes because ' + err.message)
-        }
-        else{
-            let featuredDeal = await createFeaturedDeal();
-            SpecialDeals.create({
-                featured: featuredDeal,
-                popular: getPopularDealsFromIndexes()
-            }, (err, specialDeals) => {
-                if(err){
-                    console.error('Something went wrong trying to create the special deals because ' + err.message);
-                }
-                else{
-                    console.log('The newly created deal is below');
-                    console.log(specialDeals);
-                }
-            });
-        }
-    });
+   console.log('running a task every day');
+   console.log('Getting deal length');
+   let length = await getDealAndLength();
+   console.log('length: ' + length);
+   for (let i = 0; i < 4; i++) {
+      addNumber(length);
+   }
+   SpecialDeals.remove({}, async err => {
+      if (err) {
+         console.error(
+            'Something wnet wrong when deleteing SpecialDealsIndexes because ' +
+               err.message
+         );
+      } else {
+         let featuredDeal = await createFeaturedDeal();
+         SpecialDeals.create(
+            {
+               featured: featuredDeal,
+               popular: getPopularDealsFromIndexes(),
+            },
+            (err, specialDeals) => {
+               if (err) {
+                  console.error(
+                     'Something went wrong trying to create the special deals because ' +
+                        err.message
+                  );
+               } else {
+                  console.log('The newly created deal is below');
+                  console.log(specialDeals);
+               }
+            }
+         );
+      }
+   });
+};
+function getPopularDealsFromIndexes() {
+   let popularDeals = [];
+   for (let i = 0; i < popularIndexes.length; i++) {
+      let popularIndex = popularIndexes[i];
+      popularDeals.push(globalDeals[popularIndex]);
+   }
+   console.log('popularDeals below');
+   console.log(popularDeals);
+   return popularDeals;
 }
-function getPopularDealsFromIndexes(){
-    let popularDeals = [];
-    for(let i = 0; i < popularIndexes.length; i++){
-        let popularIndex = popularIndexes[i];
-        popularDeals.push(globalDeals[popularIndex]);
-    }
-    console.log('popularDeals below');
-    console.log(popularDeals);
-    return popularDeals;
-}
-async function createFeaturedDeal(){
-    let featuredId;
-    console.log('Getting get ids of featured names');
-    featuredIds = await getIdsOfFeaturedNames();
-    console.log('Getting ids of featured titles that are on sale');
-    saleIDs = await getFeaturedIDIndexesThatAreOnSale(featuredIds);
-    let featuredIndex;
-    if(saleIDs.length <= 0){
-        console.log('No game in featured list is on sale');
-        featuredId = featuredIds[Math.floor(Math.random() * (featuredIds.length - 1))];
-    }
-    else{
-        featuredId = saleIDs[Math.floor(Math.random() * (saleIDs.length - 1))];
-    }
-    featuredIndex = featuredIds.indexOf(featuredId);
-    console.log('Getting deal from selected id');
-    let featuredDeal = await getDealFromId(featuredId);
-    featuredDeal.picture = `https://serene-williams-bb8bee.netlify.app/images/FeaturedThumbnails/${featureDealNames[featuredIndex]}.png`;
-    featuredDeal.dealID = featuredId;
-    return featuredDeal;
-}
-
-async function getIdsOfFeaturedNames(){
-    featuredIds = [];
-    for(let i = 0; i < featureDealNames.length; i++){
-        let featuredDealName = featureDealNames[i]
-        let dealId;
-        let data = await fetch(`https://www.cheapshark.com/api/1.0/games?title=${featuredDealName}`);
-        let deal = await data.json();
-        featuredIds.push(deal[0].cheapestDealID);
-    }
-    return(featuredIds);
+async function createFeaturedDeal() {
+   let featuredId;
+   console.log('Getting get ids of featured names');
+   featuredIds = await getIdsOfFeaturedNames();
+   console.log('Getting ids of featured titles that are on sale');
+   saleIDs = await getFeaturedIDIndexesThatAreOnSale(featuredIds);
+   let featuredIndex;
+   if (saleIDs.length <= 0) {
+      console.log('No game in featured list is on sale');
+      featuredId =
+         featuredIds[Math.floor(Math.random() * (featuredIds.length - 1))];
+   } else {
+      featuredId = saleIDs[Math.floor(Math.random() * (saleIDs.length - 1))];
+   }
+   featuredIndex = featuredIds.indexOf(featuredId);
+   console.log('Getting deal from selected id');
+   let featuredDeal = await getDealFromId(featuredId);
+   featuredDeal.picture = `https://serene-williams-bb8bee.netlify.app/images/FeaturedThumbnails/${featureDealNames[featuredIndex]}.png`;
+   featuredDeal.dealID = featuredId;
+   return featuredDeal;
 }
 
-async function getDealFromId(id){
-    let data = await fetch(`https://www.cheapshark.com/api/1.0/deals?id=${id}`);
-    let deal = await data.json();
-    return deal;
+async function getIdsOfFeaturedNames() {
+   featuredIds = [];
+   for (let i = 0; i < featureDealNames.length; i++) {
+      let featuredDealName = featureDealNames[i];
+      let dealId;
+      let data = await fetch(
+         `https://www.cheapshark.com/api/1.0/games?title=${featuredDealName}`
+      );
+      let deal = await data.json();
+      if (!deal[0]) {
+         continue;
+      }
+      featuredIds.push(deal[0].cheapestDealID);
+   }
+   return featuredIds;
 }
 
-async function getFeaturedIDIndexesThatAreOnSale(featuredIds){
-    saleIndexes = [];
-    console.log('featuredIds.length below');
-    console.log(featuredIds.length);
-    for(let i = 0; i < featuredIds.length; i++){
-        let featuredId = featuredIds[i];
-        let data = await fetch(`https://www.cheapshark.com/api/1.0/deals?id=${featuredId}`);
-        let deal = await data.json();
-        
-        let onSale = false;
-        if(deal.salePrice < deal.retailPrice){
-            onSale = true;
-        }
-        if(onSale){
-            saleIndexes.push(i);
-        }
-        console.log(`${i} out of ${(featuredIds.length - 1)}`);
-    }
+async function getDealFromId(id) {
+   let data = await fetch(`https://www.cheapshark.com/api/1.0/deals?id=${id}`);
+   let deal = await data.json();
+   return deal;
+}
 
-    return saleIndexes;
+async function getFeaturedIDIndexesThatAreOnSale(featuredIds) {
+   saleIndexes = [];
+   console.log('featuredIds.length below');
+   console.log(featuredIds.length);
+   for (let i = 0; i < featuredIds.length; i++) {
+      let featuredId = featuredIds[i];
+      let data = await fetch(
+         `https://www.cheapshark.com/api/1.0/deals?id=${featuredId}`
+      );
+      let deal = await data.json();
+
+      let onSale = false;
+      if (deal.salePrice < deal.retailPrice) {
+         onSale = true;
+      }
+      if (onSale) {
+         saleIndexes.push(i);
+      }
+      console.log(`${i} out of ${featuredIds.length - 1}`);
+   }
+
+   return saleIndexes;
 }
 
 let getDealAndLength = () => {
-    return new Promise((resolve, reject) => {
-        fetch('https://www.cheapshark.com/api/1.0/deals', {
-            method: 'get'
-        })
-        .then((res) => {
+   return new Promise((resolve, reject) => {
+      fetch('https://www.cheapshark.com/api/1.0/deals', {
+         method: 'get',
+      })
+         .then(res => {
             return res.json();
-        })
-        .then((deals) => {
+         })
+         .then(deals => {
             globalDeals = deals;
             resolve(deals.length);
-        })
-        .catch(err => {
-            console.error('error happeoned in get fresh deals fetch because ' + err.message);
+         })
+         .catch(err => {
+            console.error(
+               'error happeoned in get fresh deals fetch because ' + err.message
+            );
             reject(-1);
-        });
-    });
-}
+         });
+   });
+};
 
-function addNumber(max){
-    let num = Math.floor(Math.random() * max);
-    while(popularIndexes.indexOf(num) >= 0){
-        num = Math.floor(Math.random() * max);
-    }
-    popularIndexes.push(num);
+function addNumber(max) {
+   let num = Math.floor(Math.random() * max);
+   while (popularIndexes.indexOf(num) >= 0) {
+      num = Math.floor(Math.random() * max);
+   }
+   popularIndexes.push(num);
 }
